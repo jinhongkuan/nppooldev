da098e64f5e39a65312eab8062dd19cdbaaa7cd6
Kuan <kuanjh123@gmail.com>
2021-03-30 01:33:51 -0500

    ver2

--- contracts/NPTaskPool.sol
+++ contracts/NPTaskPool.sol
@@ -10,11 +10,10 @@ contract NPTaskPool
         string decision_function;
         uint reward;
         address requester;
-        uint attempts;
+        address verification_contract;
     }
 
     mapping(uint => NPTask) public getTask; 
-    mapping(uint => mapping(address => address)) public getGolem; 
     address[] public allTasks; 
     uint public taskCount;
 
@@ -23,7 +22,7 @@ contract NPTaskPool
         taskCount = 0;
     }
 
-    function createTask(string calldata name, string calldata decision_function, uint reward) external returns (uint taskIndex)
+    function createTask(string calldata name, string calldata decision_function, uint reward, address verification_contract) external returns (uint taskIndex)
     {
         // Create new smart contract that details the NP task 
         NPTask memory new_task = NPTask({
@@ -32,7 +31,7 @@ contract NPTaskPool
             decision_function: decision_function, 
             reward: reward, 
             requester: msg.sender,
-            attempts: 0});
+            verification_contract: verification_contract});
         
         getTask[new_task.index] = new_task;
 
